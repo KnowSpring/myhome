68376b04c4d1ff4c58cf376a3798c6e528926c7e
knowSpring <1521644128@qq.com>
2019-09-14 23:18:34 +0800

    增加Info.jsx

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
-import 
+import {Tab} from 'semantic-ui-react'
 class Info extends React.Component{
+  
   render(){
-    return (<div>info</div>)
+    const panes = [
+      { menuItem: 'Tab 1', render: () => <Tab.Pane>Tab 1 Content</Tab.Pane> },
+      { menuItem: 'Tab 2', render: () => <Tab.Pane>Tab 2 Content</Tab.Pane> },
+      { menuItem: 'Tab 3', render: () => <Tab.Pane>Tab 3 Content</Tab.Pane> },
+    ]
+    return (<Tab panes={panes} />)
   }
 
 }
